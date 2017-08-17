0a96e1d7ab40dc69f5579c857d1105d81acc5456
kyson <zjh171@qq.com>
2017-08-17 18:27:29 +0800

    service added

--- Watermelon/watermelon/service/WMService.m
+++ Watermelon/watermelon/service/WMService.m
@@ -13,13 +13,23 @@
 
 + (WMService *) serviceWithURL: (NSURL *) URL {
     WMService *service = [[WMService alloc] init];
-    
-    
+    NSString *decodedURLString = [self __URLDecodingWithEncodingString:URL.absoluteString];
+    NSLog(@"decodedURLString:%@",decodedURLString);
     
     return service;
     
 }
 
 
+
++ (NSString *)__URLDecodingWithEncodingString:(NSString *)encodingString
+{
+    NSMutableString *string = [NSMutableString stringWithString:encodingString];
+    [string replaceOccurrencesOfString:@"+"
+                            withString:@" "
+                               options:NSLiteralSearch
+                                 range:NSMakeRange(0, [string length])];
+    return [string stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+}
 
 @end
